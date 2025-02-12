40c548b17ead7bad846daecdb806e50d3c8bb92c
Chris Hammock <chris@crshmk.com>
2025-02-12 09:45:17 +0700

    update utils

--- src/utils/onKeyDownEnter.ts
+++ src/utils/onKeyDownEnter.ts
@@ -1,5 +1,3 @@
-// watch for enter key
-// onKeyPress={onKeyPressEnter(cb)}
 import React from 'react'
 
 type SideEffectType = () => void 
@@ -8,10 +6,20 @@ type EventHandlerType<T> = (e: React.KeyboardEvent<T>) => void
 
 type OnKeyDownEnter = <T extends HTMLElement>(cb: SideEffectType) => EventHandlerType<T>
 
+/**
+ * watch for enter key on a focused element 
+ * 
+ * @param cb side effect 
+ * 
+ * @example 
+ *   <span
+ *     tabIndex={0}
+ *     onKeyPress={onKeyPressEnter(cb)}
+ */
 const onKeyDownEnter: OnKeyDownEnter = cb => e => {
-    if (e.key === 'Enter') {
-      cb()
-    }
+  if (e.key === 'Enter') {
+    cb()
   }
+}
 
 export default onKeyDownEnter
